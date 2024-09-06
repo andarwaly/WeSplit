67b59a021e7cef9764310ae0f6f5856882508693
Dzaky Waly <andarwaly@gmail.com>
2024-09-06 16:06:01 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Muhammad Dzaky on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
